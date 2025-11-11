257b7a0c61e976ed549997203e9dd968a99f0c2d
ZainabShrifeh <zshrifeh@gmail.com>
2025-11-11 20:24:28 +0200

    initial commit

---
+++ python_exercises/ex/easy/ex3.py
@@ -0,0 +1,8 @@
+def lists_function(list1):
+    list2 = []
+    list2.append(list1[0])
+    list2.append(list1[-1])
+    print(f'element1 : {list1[0]}, element2 : {list2[1]}')
+
+lolo =[2,4,7,8,8]
+lists_function(lolo)
